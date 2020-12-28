b94a2473bb73285b095635e7f76ca4646862214a
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2020-12-28 23:32:59 +0100

    analystic

--- src/Screens.js/Home.js
+++ src/Screens.js/Home.js
@@ -1,16 +1,31 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Dimensions, Text, View} from 'react-native';
 import {useSelector} from 'react-redux';
 import moment from 'moment';
 import {
+  ScrollView,
   TouchableOpacity,
   TouchableWithoutFeedback,
 } from 'react-native-gesture-handler';
 
+import {ProgressChart} from 'react-native-chart-kit';
+
 export default function Home() {
   var allMemebersStore = useSelector((state) => state.DataReducer.allMembers);
 
+  const chartConfig = {
+    backgroundGradientFrom: '#1E2923',
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: '#08130D',
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false, // optional
+  };
+
+  const screenWidth = Dimensions.get('window').width;
   useEffect(() => {
     calculateExpiredMembers();
   }, [allMemebersStore]);
@@ -39,36 +54,75 @@ export default function Home() {
   }
 
   const navigation = useNavigation();
+
+  var Expired =
+    parseInt((expiredMembers * 100) / allMemebersStore.length) / 100;
+  var active =
+    parseInt(
+      ((allMemebersStore.length - expiredMembers) * 100) /
+        allMemebersStore.length,
+    ) / 100;
+
+  var archivied = ArchiviedMemebersStore.length;
   return (
-    <View style={{flex: 1, alignContent: 'center'}}>
+    <ScrollView style={{flex: 1, backgroundColor: '#000'}}>
       <TouchableOpacity
         style={{}}
         onPress={() => {
           navigation.openDrawer();
         }}>
-        <Text style={{fontSize: 30, fontWeight: 'bold', marginHorizontal: 15}}>
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: 'bold',
+            marginHorizontal: 15,
+            color: 'white',
+          }}>
           =
         </Text>
       </TouchableOpacity>
 
-      <View style={{flex: 1, paddingTop: 50}}>
+      <View style={{marginHorizontal: 50}}>
+        <Button
+          onPress={() => {
+            navigation.push('AddMember');
+          }}
+          title={'Add new member'}
+        />
+        <View style={{marginVertical: 10}} />
+        <Button
+          onPress={() => {
+            navigation.push('AddMember');
+          }}
+          title={'check member'}
+        />
+      </View>
+
+      <View style={{flex: 1, paddingTop: 5}}>
+        <Text>ArchiviedMemebersStore : {ArchiviedMemebersStore.length}</Text>
+
+        <View style={{marginVertical: 15}} />
+      </View>
+
+      <View style={{backgroundColor: 'white', margin: 20, borderRadius: 20}}>
         <View
           style={{
             flexDirection: 'row',
             justifyContent: 'space-around',
             height: 200,
+            margin: 20,
           }}>
           <View
             style={{
               alignItems: 'center',
               justifyContent: 'center',
             }}>
-            <Text style={{fontSize: 25}}>
+            <Text style={{fontSize: 25, color: 'black'}}>
               {allMemebersStore.length
                 ? allMemebersStore.length - expiredMembers
                 : 0}{' '}
             </Text>
-            <Text>Active</Text>
+            <Text style={{color: 'black'}}>Active</Text>
           </View>
 
           <TouchableWithoutFeedback
@@ -86,14 +140,19 @@ export default function Home() {
                 borderWidth: 5,
                 borderColor: '#52A8FB',
               }}>
-              <Text style={{fontSize: 50, marginLeft: 10}}>
+              <Text style={{fontSize: 50, marginLeft: 10, color: 'black'}}>
                 {allMemebersStore.length ? allMemebersStore.length : 0}{' '}
               </Text>
               <Text style={{fontSize: 26, color: 'grey'}}>Total</Text>
             </View>
           </TouchableWithoutFeedback>
 
-          <View style={{alignItems: 'center', justifyContent: 'center'}}>
+          <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              color: 'black',
+            }}>
             <View>
               <Text style={{color: 'black', fontSize: 25}}>
                 {expiredMembers}
@@ -103,26 +162,45 @@ export default function Home() {
             </View>
           </View>
         </View>
-        <View style={{marginVertical: 15}} />
-
-        <Text>ArchiviedMemebersStore : {ArchiviedMemebersStore.length}</Text>
-
-        <View style={{marginVertical: 15}} />
-
-        <Button
-          onPress={() => {
-            navigation.push('AddMember');
-          }}
-          title={'Add new member'}
-        />
-        <View style={{marginVertical: 10}} />
-        <Button
-          onPress={() => {
-            navigation.push('AddMember');
-          }}
-          title={'check member'}
-        />
       </View>
-    </View>
+      <View style={{marginVertical: 15}} />
+
+      <Text
+        style={{
+          color: 'white',
+          fontSize: 19,
+          fontWeight: 'bold',
+          marginLeft: 40,
+        }}>
+        Statistiques
+      </Text>
+
+      <ProgressChart
+        data={{
+          labels: ['Expired', 'active', 'archivied'], // optional
+          data: [
+            // allMemebersStore.length,
+            active,
+            Expired,
+            archivied,
+          ],
+        }}
+        width={screenWidth}
+        height={220}
+        strokeWidth={16}
+        radius={32}
+        chartConfig={{
+          backgroundGradientFrom: '#1E2923',
+          backgroundGradientFromOpacity: 0,
+          backgroundGradientTo: '#08130D',
+          backgroundGradientToOpacity: 0.5,
+          color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+          strokeWidth: 2, // optional, default 3
+          barPercentage: 0.5,
+          useShadowColorFromDataset: false, // optional
+        }}
+        hideLegend={false}
+      />
+    </ScrollView>
   );
 }
